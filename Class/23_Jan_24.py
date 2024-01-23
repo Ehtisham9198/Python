2503086795661d554ff51e12758b0cda1547cd73
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2024-01-23 11:25:52 +0530

    assignmeny

--- Class/23_Jan_24.py
+++ Class/23_Jan_24.py
@@ -58,3 +58,23 @@ print(matrix)
 #  print zeroth column element
 first_col = [row[0] for row in matrix]
 print(first_col)
+
+
+# min and max element
+print(max(list_1))
+print(max(list_2))
+print(min(list_2))
+
+# random
+import random from randint
+
+
+# Assignment
+'''
+1-Write a function that takes two sorted list as input and returns a new merged,sorted list without sort funtion
+2-Write a function that takes a list as input and return a new list without duplicate element while mainating the order
+3- Write a function to transpose a give matrix represent as a list of list
+
+
+'''
+
