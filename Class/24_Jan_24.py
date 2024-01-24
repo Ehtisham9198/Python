99b81cac78b8e7fb9d870385f2ef4ec0b7085bdd
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2024-01-24 16:17:39 +0530

    python4

---
+++ Class/24_Jan_24.py
@@ -0,0 +1,17 @@
+# Dictionary 
+# No certian order in dictionary
+
+'''
+Mapping- collection of objects that are stored by a key unlike a sequence that stored object by their relative position
+
+'''
+dic_1 = { "key1":"value1", "key2":"value2" }
+print(dic_1["key1"])
+print(dic_1["key2"])
+dic_1["key1"]=123
+
+print(dic_1["key2"].upper())
+
+print(dic_1["key1"]-100)
+
+print(dic_1.items())
