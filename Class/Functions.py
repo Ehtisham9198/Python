1980e00b3a1dec3fb882d3416fdd81c68d3e3c1d
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2024-01-31 16:02:18 +0530

    push

--- Class/Functions.py
+++ Class/Functions.py
@@ -1,1 +1,25 @@
 #               Functions
+#   A block of code that perform a specific task
+
+def Swap(a,b):
+    
+        temp =a
+        a=b
+        b=temp
+        return a,b
+
+print(Swap(2,3))
+
+def SUM(n):
+        if(n==0): return 0
+        else:
+           return (n + int(SUM(n-1)))
+
+print(SUM(10))
+
+def Factorial(n):
+      if(n==0): return 1
+      else:
+            return n*Factorial(n-1)
+
+print(Factorial(5))
