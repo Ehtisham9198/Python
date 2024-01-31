f2420508db0e811a026f34e5bca9a6c9acd99691
ehtisham mohd <125719040+Ehtisham9198@users.noreply.github.com>
2024-01-31 15:37:36 +0530

    push

---
+++ Assignment/3rd.py
@@ -0,0 +1,6 @@
+'''
+1-Write a function that takes two sorted list as input and returns a new merged,sorted list without sort funtion
+2-Write a function that takes a list as input and return a new list without duplicate element while mainating the order
+3- Write a function to transpose a give matrix represent as a list of list
+
+'''
---
+++ Class/30_Jan_24.py
@@ -0,0 +1,45 @@
+            #           Tuples
+
+t = ("one",2)
+print(t[0])
+print(t[1])
+
+# t.index()
+print(t.count(2))
+
+# NOTE-> immutable(Can't Change)
+
+
+'''
+                  SET
+it is the collection of similar type of elements, which is unordered, unchangeble,unindexed
+
+Not sorted
+
+'''
+
+list =[111,1,30,1,2,23,3]
+print(set(list))
+
+k =[2,1,2,900,3,4,5,67]
+print(set(k))
+
+k =[2,1,2,3,4,5,67]
+print(set(k))
+
+l =[11,23,11,67,67]
+print(set(l))
+
+'''
+                              BOOLIAN
+
+                
+In Python, the boolean data type is represented by the bool class. There are two built-in boolean values: True and False.
+These values are used to represent the truth or falsity of an expression or condition
+
+'''
+print(1>2)
+
+                        #Assignment
+#Set is unordered but sometimes give sorted????
+
